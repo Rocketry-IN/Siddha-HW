ce224eb79703005a81b9b76823547f554696e5cd
Sunny Gavali <54460057+sunnygavali02@users.noreply.github.com>
2023-11-05 20:21:13 +0530

    Update touchdown.ino

--- FMC/touchdown.ino
+++ FMC/touchdown.ino
@@ -1,67 +1,150 @@
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+/*
+ Rocket Touchdown Logic
+
+ Uses BMP280 pressure sensor to detect apogee and deploy parachutes.
+ Uses MPU6050 accelerometer and gyro to track orientation during descent.
+ Beeps a piezo buzzer after landing is detected.
+
+ Hardware Connections:
+
+ BMP280 - I2C
+ MPU6050 - I2C
+ chute pins - D9 and D10
+ Buzzer - D6
+
+ Created by sunnygavali02
+*/
+
+#include <Adafruit_BMP280.h> // Pressure sensor library
+#include <Adafruit_MPU6050.h> // Motion sensor library
+#include <Servo.h> // Servo library
 
 
-Adafruit_MPU6050 mpu;
-int threshold = 500;  //Taken from prev reading, Adjust according to requirement.
+// Object instances
+Adafruit_BMP280 bmp; // Pressure sensor  
+Adafruit_MPU6050 mpu; // Motion sensor
+Servo drogueChute; // Drogue parachute servo
+Servo mainChute; // Main parachute servo
 
 
+// Altitude thresholds (meters)
+const float seaLevelPressure = 1013.25;  
+int deployDrogue = 1000;   //meters
+int deployMain = 500; //meters
+
+
+int landedThreshold = 2;  //meters
+/* It represents an altitude threshold in meters above ground level.
+When the calculated altitude from the BMP280 pressure sensor drops below this threshold,
+it indicates the rocket has very nearly reached the ground.*/
+
+// Servo pins 
+const int droguePin = 9;
+const int mainPin = 10;
+const int buzzer = 6;
 
 void setup() {
-  Wire.begin();
-  mpu.begin();
-  Serial.begin(9600);
+
+  Serial.begin(9600); // Initialize serial monitor
+
+
+ pinMode(buzzer, OUTPUT);
+  // Initialize BMP280
+  if(!bmp.begin()){
+    Serial.println("BMP280 error!");
+    while(1);
+  }
+
+  // Initialize MPU6050
+  if(!mpu.begin()){ 
+    Serial.println("MPU6050 error!");
+    while(1);
+  }
+
+  // Configure MPU6050
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G); //Setting range as discussed prev
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG); //Approximately 
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);  //Approx. 
+
+  // Attach pins
+  drogueChute.attach(droguePin);  
+  mainChute.attach(mainPin);
 
 }
 
 void loop() {
 
-
-
-  // Read accelerometer data
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-  // Check acceleration in the y-axis
-  float accelY = a.acceleration.y;
-
-
-
-  // Remove noise with running average
-  float avgY;
-  avgY = (accelY*0.9) + (accelY*0.1);
+  // Read BMP280 pressure
+  float pressure = bmp.readPressure(); 
+  
+  // Calculate altitude
+  float atmospheric = pressure / seaLevelPressure;
+  float altitude = 44330 * (1.0 - pow(atmospheric, 0.1903));
 
 
 /*
-This takes 90% of the new raw value and 10% of the previous filtered value.
-For example, if the previous avgY is 500 and the new accelY is 520:
-avgY = (520*0.9) + (500*0.1) = 468 + 50 = 518
-So the new filtered value is 518 instead of the raw 520.
-Like I said, we have to play a lil bit with it to find the ideal value!
+The key formula is:
+
+altitude = 44330 * (1 - (pressure / seaLevelPressure)^0.1903)
+
+Where:
+
+    44330 is a constant representing altitude change per pressure change.
+    seaLevelPressure is assumed pressure at sea level.
+    pressure is measured ambient pressure.
+    (pressure / seaLevelPressure)^0.1903 applies a standard atmosphere model.
+
+In summary:
+
+    It calculates the pressure ratio compared to sea level.
+    Applies an atmospheric model exponent.
+    Scales the result into meters of altitude based on a pressure-altitude constant.
+
+So it converts the pressure reading to an altitude estimate using standard atmosphere modeling.
+
+Thanks to GPT!!!
 
 */
+  // Check if pressure decreased
+  static float prevPressure;
+  bool isDecreasing = pressure < prevPressure;
+  prevPressure = pressure;
 
-
-  if(avgY > threshold) {
-    Serial.println("Ascending"); 
+  // Detect apogee
+  if(isDecreasing == false){
+    Serial.println("Apogee reached!"); 
+    drogueChute.write(180); // Release drogue
   }
 
-
-  else if(avgY < -threshold) {
-    Serial.println("Descending");
+  // Deploy main chute  
+  if(altitude < deployMain && isDecreasing){
+    Serial.println("Deploying main chute!");
+    mainChute.write(180); 
   }
 
-
-  else {
-    Serial.println("Apogee");
+  // Detect landing
+  if(altitude < landedThreshold){
+    Serial.println("Landed!");
+    
+    // Beep every 1 sec
+    while(1){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000); 
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000); 
+    }
   }
 
-
-
-  delay(10);
-
-
-
- 
+  // Print motion data
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  Serial.print(a.acceleration.x);
+  Serial.print(",");
+  Serial.print(a.acceleration.y); 
+  Serial.print(",");
+  Serial.print(g.gyro.x);
+  Serial.print(",");
+  Serial.println(g.gyro.y);
+  
+  delay(100);
 }
